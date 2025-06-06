a270abb88b73fb68ab4b8fbb42dceeb1e2bc91f1
Poghossian <mariam.poghossian@etu.univ-amu.fr>
2025-06-06 11:37:54 +0200

    Initial commit

---
+++ src/main/java/fr/amu/iut/saesuper_bomberman/HelloController.java
@@ -0,0 +1,14 @@
+package fr.amu.iut.saesuper_bomberman;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module fr.amu.iut.saesuper_bomberman {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens fr.amu.iut.saesuper_bomberman to javafx.fxml;
+    exports fr.amu.iut.saesuper_bomberman;
+}
