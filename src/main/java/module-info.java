3459b367c09e7b0b68bb85d9565449b0d24ef9bf
mariampoghossian <poghossianm@gmail.com>
2025-06-11 22:30:30 +0200

    merge branche Game dans menuPropre

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,18 @@
 module fr.amu.iut.saesuper_bomberman {
+    // Exports existants
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
     requires javafx.media;
 
-    exports fr.amu.iut.saesuper_bomberman;
-    exports fr.amu.iut.saesuper_bomberman.components;
-    exports fr.amu.iut.saesuper_bomberman.controllerfxml;
-
     opens fr.amu.iut.saesuper_bomberman to javafx.fxml;
-    opens fr.amu.iut.saesuper_bomberman.components to javafx.fxml;
     opens fr.amu.iut.saesuper_bomberman.controllerfxml to javafx.fxml;
+    opens fr.amu.iut.saesuper_bomberman.components to javafx.fxml;
+
+    // Ajouter ces lignes pour le package model
+    opens fr.amu.iut.saesuper_bomberman.model to javafx.fxml;
+    exports fr.amu.iut.saesuper_bomberman.model;
+
+    exports fr.amu.iut.saesuper_bomberman;
+    exports fr.amu.iut.saesuper_bomberman.controllerfxml;
+    exports fr.amu.iut.saesuper_bomberman.components;
 }
