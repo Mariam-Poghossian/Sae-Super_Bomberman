b1b4cb202dd55c3cf829e4fe721982ede60ea7c7
Amara <sinda.amara@etu.univ-amu.fr>
2025-06-12 00:42:01 +0200

    commit de la javadoc de certaines classes

--- src/main/java/fr/amu/iut/saesuper_bomberman/components/ControlSettingsController.java
+++ src/main/java/fr/amu/iut/saesuper_bomberman/components/ControlSettingsController.java
@@ -4,37 +4,53 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+/**
+ * Contrôleur pour la fenêtre des paramètres de contrôle des joueurs.
+ * Permet de sélectionner un joueur et d'afficher les touches associées à ses contrôles.
+ */
 public class ControlSettingsController {
 
     @FXML
     private TextField leftKey, rightKey, upKey, downKey, bombKey;
-
+    /**
+     * Numéro du joueur actuellement sélectionné (1 à 4).
+     */
     private int currentPlayer = 1;
-
+    /**
+     * Sélectionne le joueur 1 et met à jour l'affichage des touches.
+     */
     @FXML
     public void selectPlayer1() {
         currentPlayer = 1;
         updateKeysDisplay();
     }
-
+    /**
+     * Sélectionne le joueur 2 et met à jour l'affichage des touches.
+     */
     @FXML
     public void selectPlayer2() {
         currentPlayer = 2;
         updateKeysDisplay();
     }
-
+    /**
+     * Sélectionne le joueur 3 et met à jour l'affichage des touches.
+     */
     @FXML
     public void selectPlayer3() {
         currentPlayer = 3;
         updateKeysDisplay();
     }
-
+    /**
+     * Sélectionne le joueur 4 et met à jour l'affichage des touches.
+     */
     @FXML
     public void selectPlayer4() {
         currentPlayer = 4;
         updateKeysDisplay();
     }
-
+    /**
+     * Met à jour l'affichage des touches selon le joueur sélectionné.
+     */
     private void updateKeysDisplay() {
         switch(currentPlayer) {
             case 1:
@@ -67,7 +83,9 @@ public class ControlSettingsController {
                 break;
         }
     }
-
+    /**
+     * Ferme la fenêtre des paramètres de contrôle.
+     */
     @FXML
     public void closeWindow() {
         Stage stage = (Stage) leftKey.getScene().getWindow();
